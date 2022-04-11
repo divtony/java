e762745a5c6cdcd3aedb08e2ecdfd44b30a864c9
divtony <taelover52@naver.com>
2022-04-11 12:36:12 +0900

    2022.04.11

---
+++ Chapter01/src/Exam_03.java
@@ -0,0 +1,10 @@
+/*
+ * 자바는 세미콜론으로 끝나야 한줄을 인식한다.
+ * */
+
+
+public class Exam_03 {
+	public static void main(String[] args) {
+		System.out.println("세미클론");
+	}
+}
---
+++ Chapter02/src/Exam_02.java
@@ -0,0 +1,25 @@
+/*
+ * 자바는 자료형을 선언해줘야한다.
+ * 같은 자료형으로 묶어서 선언도 가능하다.
+ * 
+ * 가독성이 좋진 않아서 선호하지 않음
+ * 
+ * */
+public class Exam_02 {
+	public static void main(String[] args) {
+		// int a,b,c;
+		
+		int a;
+		int b;
+		int c;
+		
+		a = 1;
+		b = 2;
+		c = 3;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+}
