cd0d754906975566cf459e3c9399feb59f107c03
divtony <taelover52@naver.com>
2022-04-17 19:51:47 +0900

    7번 과제까지 완료

--- Program01/src/Program_07.java
+++ Program01/src/Program_07.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*
  * 성적관리 프로그램
  * 
@@ -52,5 +56,152 @@
  * 3. 메뉴에서 작업종료를 입력하기 전까지는 메뉴를 계속반복하여 출력한다.
  * */
 public class Program_07 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader (new InputStreamReader(System.in));
+		int kor = 0;
+		int math = 0;
+		int eng = 0;
+		int sum = 0;
+		float avg;
+		int grade;
+		
+		while(true) {
+			System.out.println("==== Grade Menu ====");
+			System.out.println("1. 국어");
+			System.out.println("2. 수학");
+			System.out.println("3. 영어");
+			System.out.println("4. 총점");
+			System.out.println("5. 평균");
+			System.out.println("6. 학점");
+			System.out.println("7. 작업종료");
+			System.out.print("선택 = ");
+			int menu = Integer.parseInt(in.readLine());
+			
+			
+		
+			
+			switch(menu) {
+		
+			case 1: {
+				do {
+					System.out.print("국어 점수 = ");
+					kor = Integer.parseInt(in.readLine());
+				} while (kor < 0 || kor > 100 );
+				kor+= 0;
+				System.out.println("성적 입력 완료!");
+				break;
+			} 
+			case 2: { 
+				do {
+					System.out.print("수학 점수 = ");
+					math = Integer.parseInt(in.readLine());
+				} while (math < 0 || math > 100 );
+				math += 0;
+				System.out.println("성적 입력 완료!");
+				break;
+				
+			}
+			 case 3: { 
+				do {
+					System.out.print("영어 점수 = ");
+					eng = Integer.parseInt(in.readLine());
+				} while (eng < 0 || eng > 100 );
+				eng += 0;
+				System.out.println("성적 입력 완료!");
+				break;
+			
+			} case 4 : {
+			
+				if (kor <= 0 && math <= 0 && eng <=0)  {
+					System.out.println("점수를 입력해주세요.");
+		
+				} else if (kor <= 0) {
+					System.out.println("국어 점수를 입력해주세요.");
+			
+				} else if (math <= 0) {
+					System.out.println("수학 점수를 입력해주세요.");
+				
+				} else if (eng <= 0) {
+					System.out.println("영어 점수를 입력해주세요.");
+		
+				}
+				else { 
+					sum = kor + math + eng;
+					System.out.println("성적 합계는 " + sum + "점 입니다.");	
+				}
+				break;
+				}
+			case 5 : {
+				if (kor <= 0 && math <= 0 && eng <=0)  {
+					System.out.println("점수를 입력해주세요.");
+		
+				} else if (kor <= 0) {
+					System.out.println("국어 점수를 입력해주세요.");
+			
+				} else if (math <= 0) {
+					System.out.println("수학 점수를 입력해주세요.");
+				
+				} else if (eng <= 0) {
+					System.out.println("영어 점수를 입력해주세요.");
+		
+				}
+				else { 
+					sum = kor + math + eng;
+					avg = sum / 3.0f;
+					System.out.printf("성적 평균은 %.2f" + "점 입니다.", avg);
+					System.out.println();
+			} break;
+				}
+				
+			case 6: {
+				if (kor <= 0 && math <= 0 && eng <=0)  {
+					System.out.println("점수를 입력해주세요.");
+		
+				} else if (kor <= 0) {
+					System.out.println("국어 점수를 입력해주세요.");
+			
+				} else if (math <= 0) {
+					System.out.println("수학 점수를 입력해주세요.");
+				
+				} else if (eng <= 0) {
+					System.out.println("영어 점수를 입력해주세요.");
+		
+				}
+				else { 
+					sum = kor + math + eng;
+					grade = sum / 3;
+					
+					if (grade >= 95) {
+						System.out.println("학점은 A+ 입니다.");
+					} else if (grade >= 90 && grade < 95) {
+						System.out.println("학점은 A 입니다.");
+					}  else if (grade >= 85 && grade < 90) {
+						System.out.println("학점은 B+ 입니다.");
+					} else if (grade >= 80 && grade < 85) {
+						System.out.println("학점은 B 입니다.");
+					}  else if (grade >= 75 && grade < 80) {
+						System.out.println("학점은 C+ 입니다.");
+					} else if (grade >= 70 && grade < 75) {
+						System.out.println("학점은 C 입니다.");
+					} else if (grade >= 65 && grade < 70) {
+						System.out.println("학점은 D+ 입니다.");
+					} else if (grade >= 60 && grade < 65) {
+						System.out.println("학점은 D 입니다.");
+					} else {
+						System.out.println("학점은 F 입니다.");
+					}
+			} break;
+			}
+			
+			case 7: {
+					System.out.println("프로그램을 종료합니다.");
+					System.exit(0);
+					break;
+			}
+	
+		}
+		
+}
 
+}
 }
